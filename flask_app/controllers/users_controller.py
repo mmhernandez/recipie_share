230c89723bcaf9fab0ed4570f2052f007bfb1d68
mmhernandez <mmdangelo11@gmail.com>
2023-02-11 21:45:34 -0700

    Implement registration validation

--- flask_app/controllers/users_controller.py
+++ flask_app/controllers/users_controller.py
@@ -1,9 +1,27 @@
 from flask_app import app
-from flask import render_template, session, redirect
+from flask import render_template, session, redirect, request
+from flask_app.models import user
 
 @app.route("/")
 def login_registration():
     return render_template("/login_registration.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if user.User.validate_registration(request.form):
+        session["id"] = user.User.insert(request.form)
+        return redirect("/all_recipies")
+    else:
+        session["first_name"] = request.form["first_name"]
+        session["last_name"] = request.form["last_name"]
+        session["email"] = request.form["email"]
+        return redirect("/")
+
+@app.route("/login", methods=["POST"])
+def login():
+    return redirect("/all_recipies")
+
+
 
 @app.route("/clear_form")
 def clear_form():
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,6 +1,10 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_bcrypt import Bcrypt
+import re
 
 db = "recipe_share"
+email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class User:
     def __init__(self, data):
@@ -10,5 +14,52 @@ class User:
         self.email = data["email"]
         self.password = data["password"]
         self.recipies = []
+
+    @staticmethod
+    def validate_registration(data):
+        is_valid = True
+
+        #first name validation
+        if len(data["first_name"]) < 2:
+            flash("First name must be at least 2 characters", "first_name")
+            is_valid = False
+
+        #last name validation
+        if len(data["last_name"]) < 2:
+            flash("Last name must be at least 2 characters", "last_name")
+            is_valid = False
+
+        #email validation
+        #check for valid format
+        if not email_regex.match(data["email"]):
+            flash("Invalid email", "email")
+            is_valid = False
+        elif User.get_one_by_email({"email": data["email"]}):
+            flash("Email already in use", "email")
+            is_valid = False
+
+        #password validation
+
+        return is_valid
+
+    @classmethod
+    def get_one_by_email(cls, data):
+        query = '''
+            SELECT * 
+            FROM users
+            WHERE email = %(email)s;
+        '''
+        results = connectToMySQL(db).query_db(query, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def insert(cls, data):
+        query = '''
+            INSERT INTO users (first_name, last_name, email, password)
+            VALUES (%(first_name)s, %(last_name)s, %(email)s, %(passord)s);
+        '''
+        return connectToMySQL(db).query_db(query, data)
 
     
